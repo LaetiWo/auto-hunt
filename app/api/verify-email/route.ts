a6cc3f0840f05944754cb7ca912f97e66e4bad50
laetiWo <laetiwo@gmail.com>
2026-05-12 11:37:16 +0300

    verifyprod

--- app/api/verify-email/route.ts
+++ app/api/verify-email/route.ts
@@ -1,14 +1,32 @@
-import { createSessionClient } from "@/lib/appwrite";
+// import { createSessionClient } from "@/lib/appwrite";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export async function POST(req: NextRequest) {
+//   try {
+//     const { userId, secret } = await req.json();
+
+//     const { account } = await createSessionClient();
+//     await account.updateVerification(userId, secret);
+
+//     return NextResponse.json({ message: "Email vérifié" });
+//   } catch (error: any) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// }
+
+import { createAdminClient } from "@/lib/appwrite";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
     const { userId, secret } = await req.json();
-    const { account } = await createSessionClient();
-    await account.updateVerification(userId, secret);
+
+    const { users } = await createAdminClient();
+    await users.updateEmailVerification(userId, true);
 
     return NextResponse.json({ message: "Email vérifié" });
   } catch (error: any) {
+    console.error("VERIFY EMAIL error:", error.message);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
